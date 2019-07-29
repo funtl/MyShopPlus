0346de04fee04d3bae8d878f6962ab68e4357888
Lusifer <topsale@vip.qq.com>
2019-07-29 16:21:55 +0800

    封装 OKHttp3 请求令牌

--- business/business-oauth2/src/main/java/com/funtl/myshop/plus/business/controller/LoginController.java
+++ business/business-oauth2/src/main/java/com/funtl/myshop/plus/business/controller/LoginController.java
@@ -6,11 +6,14 @@ import com.funtl.myshop.plus.commons.utils.MapperUtils;
 import com.funtl.myshop.plus.commons.utils.OkHttpClientUtil;
 import com.google.common.collect.Maps;
 import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -30,25 +33,40 @@ public class LoginController {
 
     private static final String URL_OAUTH_TOKEN = "http://localhost:9001/oauth/token";
 
+    @Value("${business.oauth2.grant_type}")
+    public String oauth2_grant_type;
+
+    @Value("${business.oauth2.client_id}")
+    public String oauth2_client_id;
+
+    @Value("${business.oauth2.client_secret}")
+    public String oauth2_client_secret;
+
     @PostMapping(value = "/user/login")
-    public ResponseResult<Map<String, Object>> login(@RequestBody LoginParam loginParam) throws Exception {
-        // 封装返回的结果集
+    public ResponseResult<Map<String, Object>> login(@RequestBody LoginParam loginParam) {
+		// 封装返回的结果集
         Map<String, Object> result = Maps.newHashMap();
 
-        // 通过 HTTP 客户端请求登录接口
+		// 通过 HTTP 客户端请求登录接口
         Map<String, String> params = Maps.newHashMap();
         params.put("username", loginParam.getUsername());
         params.put("password", loginParam.getPassword());
-        params.put("grant_type", "password");
-        params.put("client_id", "client");
-        params.put("client_secret", "secret");
+        params.put("grant_type", oauth2_grant_type);
+        params.put("client_id", oauth2_client_id);
+        params.put("client_secret", oauth2_client_secret);
 
-        // 解析响应结果封装并返回
-        Response response = OkHttpClientUtil.getInstance().postData(URL_OAUTH_TOKEN, params);
-        String jsonString = response.body().string();
-        Map<String, Object> jsonMap = MapperUtils.json2map(jsonString);
-        String token = String.valueOf(jsonMap.get("access_token"));
-        result.put("token", token);
+        try {
+			// 解析响应结果封装并返回
+            Response response = OkHttpClientUtil.getInstance().postData(URL_OAUTH_TOKEN, params);
+            String jsonString = response.body().string();
+            Map<String, Object> jsonMap = MapperUtils.json2map(jsonString);
+            String token = String.valueOf(jsonMap.get("access_token"));
+            result.put("token", token);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return new ResponseResult<Map<String, Object>>(20000, "登录成功", result);
     }
