5c53e279fff45a33e9afdaf1fd9aed8689453d1c
李卫民 <topsale@vip.qq.com>
2019-10-15 22:35:59 +0800

    实现功能 基于 Redis 存储令牌

--- business/business-oauth2-service/src/main/java/com/funtl/myshop/plus/business/configure/AuthorizationServerConfiguration.java
+++ business/business-oauth2-service/src/main/java/com/funtl/myshop/plus/business/configure/AuthorizationServerConfiguration.java
@@ -22,6 +22,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -32,7 +33,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import javax.sql.DataSource;
 
@@ -60,6 +61,9 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
     @Bean
     @Primary
     @ConfigurationProperties(prefix = "spring.datasource")
@@ -71,7 +75,8 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Bean
     public TokenStore tokenStore() {
         // 基于 JDBC 实现，令牌保存到数据库
-        return new JdbcTokenStore(dataSource());
+//        return new JdbcTokenStore(dataSource());
+        return new RedisTokenStore(redisConnectionFactory);
     }
 
     @Bean
